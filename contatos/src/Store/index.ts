e837c72396e37131940a52d51a0d072c7198a9f1
Elias-Vasconcelos <146135801+Elias-Vasconcelos@users.noreply.github.com>
2024-06-28 09:09:38 -0300

    Criacao do filtro Slice

--- contatos/src/Store/index.ts
+++ contatos/src/Store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import contatosSlice from './reducers/Contatos'
+import FiltroSlice from './reducers/Filtro'
 
 const store = configureStore({
   reducer: {
-    Contatos: contatosSlice.reducer
+    Contatos: contatosSlice.reducer,
+    Filtro: FiltroSlice
   }
 })
 
---
+++ contatos/src/Store/reducers/Filtro.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import * as enums from '../../utis/enums/index'
+
+type FiltroType = {
+    termo: string
+    tipo: 'todos' | enums.Filiacao
+}
+
+const initialState: FiltroType ={
+    termo: '',
+    tipo: 'todos'
+}
+
+const FiltroSlice = createSlice({
+    name: 'Filtro',
+    initialState,
+    reducers: {
+        SetTermo: (state, action: PayloadAction<string> ) => {
+            state.termo = action.payload
+        }
+    }
+})
+
+export const { SetTermo } = FiltroSlice.action
+export default FiltroSlice.reducer
