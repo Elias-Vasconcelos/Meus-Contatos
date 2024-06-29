b9ac251ae6685b786a7bbed30c544c9ed5d8b1fd
Elias-Vasconcelos <146135801+Elias-Vasconcelos@users.noreply.github.com>
2024-06-29 13:03:36 -0300

    Correçoes

--- contatos/src/container/CadastraContato/index.tsx
+++ contatos/src/container/CadastraContato/index.tsx
@@ -10,32 +10,32 @@ const CadastraTarefas = (evento: FormEvent) => {
     const [ userName, setuserName ] = useState('')
     const [ userEmail, setuserEmail ] = useState('')
     const [ userNumber, setuserNumber ] = useState(0)
-    const [ userFiliacao, setuserFiliacao ] = useState(0)
-
+    const [ userFiliacao, setuserFiliacao ] = useState(enums.Filiacao)
+    
     return (
         < ContainerB >
          <Formulario>
             <Titulo>Novo contato</Titulo>
             <div style={{ width: 600, display: 'flex', justifyContent: 'center' }}>
             <label htmlFor="nome"> Nome Completo: </label>
-            <input id='nome' type="text" onchange={evento => setuserName(evento.target.value)} />
+            <input id='nome' type="text" onChange={evento => setuserName(evento.target.value)} />
             </div>
             <div style={{ width: 600, display: 'flex', justifyContent: 'center' }} >
             <label htmlFor="E-mail"> E-mail: </label>
-            <input id='E-mail' type="text" onchange={evento => setuserEmail(evento.target.value)} />
+            <input id='E-mail' type="text" onChange={evento => setuserEmail(evento.target.value)} />
             </div>
             <div style={{ width: 600, display: 'flex', justifyContent: 'center' }} >
             <label htmlFor="Telefone"> Telefone: </label>
-            <input id='Telefone' type="number" onchange={evento => setuserNumber(evento.target.value)} />
+            <input id='Telefone' type="number" onChange={evento => setuserNumber(evento.target.value)} />
             </div>
          </Formulario>
          <Formulario>
          <Titulo>Tipo de contato:</Titulo>
           <div>
-            <Seletor>Trabalho</Seletor>
-            <Seletor>Comercial</Seletor>
-            <Seletor>Familiar</Seletor>
-            <Seletor>Amigos</Seletor>
+            <Seletor ativo={SetSeletor} onClick={(evento) => setuserFiliacao(enums.Filiacao.Trabalho)} >Trabalho</Seletor>ativo={} 
+            <Seletor ativo={SetSeletor} onClick={(evento) => setuserFiliacao(enums.Filiacao.Comercial)} >Comercial</Seletor>ativo={} 
+            <Seletor ativo={SetSeletor} onClick={(evento) => setuserFiliacao(enums.Filiacao.Familiar)} >Familiar</Seletor>ativo={} 
+            <Seletor ativo={SetSeletor} onClick={(evento) => setuserFiliacao(enums.Filiacao.Amigos)} >Amigos</Seletor>
           </div>
          </Formulario>
         </ContainerB>
